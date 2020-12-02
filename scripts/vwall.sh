bfee2eab8a6eadd04dcb22d7c22fd436be82b980
Johannes Wünsche <johannes.wuensche97@gmail.com>
2020-12-02 10:58:53 +0100

    Change interface name vwall pub ip script

--- scripts/vwall.sh
+++ scripts/vwall.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+# This script is based on https://doc.ilabt.imec.be/ilabt/virtualwall/network.html#address-pool and has
+# been adapted to be used with mutltiple IPs on Virtual Wall 2
 set -euo pipefail
 
 if test "${USER}" != "root"
@@ -13,7 +16,7 @@ setup_public_ip() {
 
     GENI_GET_INFO='/tmp/geni-get-info.py'
 
-    curl -s https://www.wall2.ilabt.iminds.be/geni-get-info.py -O "${GENI_GET_INFO}" || exit 10
+    curl -s https://www.wall2.ilabt.iminds.be/geni-get-info.py > "${GENI_GET_INFO}" || exit 10
     chmod u+x "${GENI_GET_INFO}"
     pub_dev=$(${GENI_GET_INFO} controldevname)
     if test "$(${GENI_GET_INFO} pubip | wc -l)" -gt 1
@@ -37,15 +40,14 @@ setup_public_ip() {
     echo 8021q | sudo tee -a /etc/modules
     cat >> /etc/network/interfaces <<EOF
 
-    auto ${pub_dev}.${vlan}
-    iface ${pub_dev}.${vlan} inet static
+    auto vlan${vlan}
+    iface vlan${vlan} inet static
     address ${pub_ip}
     netmask 255.255.255.192
     gateway ${gateway}
     vlan-raw-device ${pub_dev}
     pre-up route del default gw ${GW}
     post-down route add default gw ${gateway}
-
 EOF
 
     # echo "Configured Public IP is: ${pub_ip}"
